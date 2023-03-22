5e8cc4c0dcf81154a8d2b00043c6dfb86b6f7950
Darius Pažusinskis <darius.pazusinskis@swedbank.lt>
2023-03-22 23:51:03 +0200

    initial commit

---
+++ src/app/shared/data.ts
@@ -0,0 +1,41 @@
+export const moviesList = {
+  "categories": [
+    { "id": 1, "title": "Action" },
+    { "id": 2, "title": "Comedy" },
+    { "id": 3, "title": "Drama" },
+    { "id": 4, "title": "Sci-Fi" },
+    { "id": 5, "title": "Horror" }
+  ],
+  "movies": [
+    {
+      "id": 1,
+      "title": "Trending now 1",
+      "imageUrl": "../assets/img.png",
+      "categoryId": 1
+    },
+    {
+      "id": 2,
+      "title": "Trending now 2",
+      "imageUrl": "../assets/img.png",
+      "categoryId": 2
+    },
+    {
+      "id": 3,
+      "title": "Trending now 3",
+      "imageUrl": "../assets/img.png",
+      "categoryId": 3
+    },
+    {
+      "id": 4,
+      "title": "Trending now 4",
+      "imageUrl": "../assets/img.png",
+      "categoryId": 4
+    },
+    {
+      "id": 5,
+      "title": "Trending now 5",
+      "imageUrl": "../assets/img.png",
+      "categoryId": 5
+    }
+  ]
+}
